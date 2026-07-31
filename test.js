b7d2a528370d41b3f427e84789a4761a5d35eae2
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-07-31 09:26:49 +0530

    export import and spread

---
+++ test.js
@@ -0,0 +1,12 @@
+import {sum, add} from "./demo4.js";
+
+console.log("Sum = ", sum(1));
+console.log("Sum = ", sum(1, 2));
+console.log("Sum = ", sum(1,2,3));
+console.log("Sum = ", sum(1,2,3,4));
+console.log();
+
+console.log("Add = ", add(1));
+console.log("Add = ", add(1, 2));
+console.log("Add = ", add(1,2,3));
+console.log("Add = ", add(1,2,3,4));
