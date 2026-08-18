06fa5aafddb7a401da977123bda47a13ec82e629
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-08-18 14:18:13 +0530

    fetch demo

--- demo5.js
+++ demo5.js
@@ -4,7 +4,7 @@ async function loadProduct() {
   try {
     const res = await fetch(URL);
     const data = await res.json();
-    console.log("res", res);
+    console.log("res", res);-+
     console.log("Data", data);
     const tbody = document.getElementById("data");
     data.products.map((i, idx) => {
