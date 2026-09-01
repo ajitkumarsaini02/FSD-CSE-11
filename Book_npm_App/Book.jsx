dfa6b2bc4392b776a1a59a5c0aa53dd3f6c7b971
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-09-01 15:00:13 +0530

    book app using react

--- Book_npm_App/Book.jsx
+++ Book_npm_App/Book.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Book() {
     return(
         <div>
-            <img src="" width={100} height={100} alt="Book Image"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3iAlq8sUCYrF044d-ct5Sy0QDcObhNEb7NvJr3jy-1g&s=10" width={100} height={100} alt="Book Image"/>
             <h2>Title: ReactJS</h2>
             <h2>Price: 465</h2>
             <button>Add to cart</button>
