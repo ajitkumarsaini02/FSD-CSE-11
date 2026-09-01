008f7d388a30ec737dd47d2a162ed5c842530ce6
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-09-01 14:57:41 +0530

    book app using react

---
+++ Book_npm_App/Book.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function Book() {
+    return(
+        <div>
+            <img src="" width={100} height={100} alt="Book Image"/>
+            <h2>Title: ReactJS</h2>
+            <h2>Price: 465</h2>
+            <button>Add to cart</button>
+        </div>
+    )
+}
+
+export default Book;
