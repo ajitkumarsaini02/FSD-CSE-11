728776931e518f12bcdc462756e05e1289fa22e5
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-09-22 15:49:30 +0530

    my shopping app

---
+++ MyShoppingApp/client/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Suspense fallback={<p>Loading products...</p>}>
+      <App />
+    </Suspense>
+  </StrictMode>,
+)
